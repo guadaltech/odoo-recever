1081ffb09b8ffbbcac612995cc60a8f7bce8707c
Javier Llamas <franciscojavier.llamas@guadaltech.es>
2019-10-03 15:26:24 +0200

    [MOD]Change name company

--- gst_recever/models/__init__.py
+++ gst_recever/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 Guadaltech Soluciones Tecnológicos S.L (<http://guadaltech.es>)
+# Copyright 2019 Guadaltech Soluciones Tecnológicas S.L (<http://guadaltech.es>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from . import pos_recever

701bba557e1366b6457aa565ce5c982820ef95e3
Javier Llamas <franciscojavier.llamas@guadaltech.es>
2019-10-03 15:21:23 +0200

    [MOD]Change name company

--- gst_recever/wizard/__init__.py
+++ gst_recever/wizard/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
-# Part of Softhealer Technologies.
+# Copyright 2019 Guadaltech Soluciones Tecnologicas S.L (<http://guadaltech.es>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from . import custom_wizard
--- gst_recever/wizard/custom_wizard.py
+++ gst_recever/wizard/custom_wizard.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 Guadaltech Soluciones Tecnológicos S.L (<http://guadaltech.es>)
+# Copyright 2019 Guadaltech Soluciones Tecnologicas S.L (<http://guadaltech.es>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 from odoo import api, fields, models, _
 
